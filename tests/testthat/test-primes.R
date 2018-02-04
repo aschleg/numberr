d6480045f4a4c1b94e8d3871737d3d1c020de701
aschleg <aaron@aaronschlegel.com>
2018-02-03 21:22:35 -0800

    remove doc, modify prime test, commit latest exports

--- tests/testthat/test-primes.R
+++ tests/testthat/test-primes.R
@@ -4,7 +4,3 @@ test_that('Naive prime test function accurately determines prime integers', {
   expect_equal(isprime(11), TRUE)
   expect_equal(isprime(9), FALSE)
 })
-
-test_that('Fermat prime test accurately determines prime integers', {
-  expect_equal(fermat_prime(11), TRUE)
-})
